51c3cb489e3f3256c96573ef596701e5d4366c5a
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-10-12 21:06:46 +0200

    Add functions addMessage, sendMessage / Remove the hard-coded messages from the HTML / AutoComplete-off

--- client/app.js
+++ client/app.js
@@ -1,26 +1,61 @@
-// References to HTML elements
+{
+    "use strict";
 
-const loginForm = document.querySelector('#welcome-form');
-const messagesSection = document.querySelector('#messages-section');
-const messagesList = document.querySelector('#messages-list');
-const addMessageForm = document.querySelector('#add-messages-form');
-const userNameInput = document.querySelector('#username');
-const messageContentInput = document.querySelector('#message-content');
+    // References to HTML elements
 
-// Global variables
+    const loginForm = document.querySelector('#welcome-form');
+    const messagesSection = document.querySelector('#messages-section');
+    const messagesList = document.querySelector('#messages-list');
+    const addMessageForm = document.querySelector('#add-messages-form');
+    const userNameInput = document.querySelector('#username');
+    const messageContentInput = document.querySelector('#message-content');
 
-let userName = '';
+    // Global variables
 
-// Functions
+    let userName = '';
 
-const login = (e) => {
-    e.preventDefault();
-    if (!userNameInput.value) {
-        alert('Please enter a username');
-    } else {
-        userName = userNameInput.value;
-        loginForm.classList.remove('show');
-        messagesSection.classList.add('show');
-        addMessageForm.classList.add('show');
+    // Functions
+
+    const login = (e) => {
+        e.preventDefault();
+        if (!userNameInput.value) {
+            alert('Please, enter a username!');
+        } else {
+            userName = userNameInput.value;
+            loginForm.classList.remove('show');
+            messagesSection.classList.add('show');
+        }
+    };
+
+    const addMessage = (author, content) => {
+        const message = document.createElement('li');
+        message.classList.add('message');
+        message.classList.add('message--received');
+        if (author === userName) message.classList.add('message--self');
+        message.innerHTML = `
+          <h3 class="message__author">${userName === author ? 'You' : author}</h3>
+          <div class="message__content">
+            ${content}
+          </div>
+        `;
+        messagesList.appendChild(message);
     }
-};
+
+    const sendMessage = (e) => {
+        e.preventDefault();
+        let messageContent = messageContentInput.value;
+        if (!messageContentInput.value) {
+            alert('Please, enter a message!');
+        } else {
+            addMessage(userName, messageContent);
+            socket.emit('message', { author: userName, content: messageContent });
+            messageContentInput.value = '';
+        }
+    };
+
+    // Event listeners
+
+    loginForm.addEventListener("submit", (e) => login(e));
+    addMessageForm.addEventListener("submit", (e) => sendMessage(e));
+
+}
