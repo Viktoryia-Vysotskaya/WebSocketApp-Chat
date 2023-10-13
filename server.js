2f661b6014f37225dbe83911744af5cab6b1dd32
Viktoryia Vysotskaya <radevich.vika2014@gmail.com>
2023-10-13 09:53:49 +0200

    Handle the received message - server as an intermediary

--- server.js
+++ server.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
+const socket = require('socket.io');
 
 const app = express();
 
 const messages = [];
+let users = [];
 
 app.use(express.static(path.join(__dirname, '/client')));
 
@@ -17,4 +19,19 @@ app.use((req, res) => {
 
 const server = app.listen(8000, () => {
     console.log('Server is running on port: 8000');
+});
+
+const io = socket(server);
+
+io.on('connection', (socket) => {
+    console.log('New client! Its id – ' + socket.id);
+    socket.on('message', () => { console.log('Oh, I\'ve got something from ' + socket.id) });
+    socket.on('disconnect', () => { console.log('Oh, socket ' + socket.id + ' has left') });
+    console.log('I\'ve added a listener on message and disconnect events \n');
+    
+    socket.on('message', (message) => {
+        console.log('Oh, I\'ve got something from ' + socket.id);
+        messages.push(message);
+        socket.broadcast.emit('message', message);
+    });
 });
